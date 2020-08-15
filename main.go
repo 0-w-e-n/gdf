e5e0405d93bc35399e37e1ad2cbd75fad7625f3a
owen <owen@localhost.localdomain>
2020-08-15 00:46:15 -0400

    test with 1MM rows

--- main.go
+++ main.go
@@ -5,24 +5,18 @@ import (
 )
 
 func main() {
-    vals := make(map[string]interface{})
-    vals["test1"] = 1
-    vals["test2"] = 10.1
-    vals["test"] = "oh boy"
-
-    vals2 := make(map[string]interface{})
-    vals2["test1"] = 12
-    vals2["test2"] = 19.19
-    vals2["test"] = ":)"
-
-    cols := []string{"test1", "test2", "test"}
+    cols := []string{"A", "B", "C"}
     types := []string{"int", "float64", "string"}
-    r := gdf.NewRow(cols, vals, types)
-    r.Show()
-    r2 := gdf.NewRow(cols, vals2, types)
-    r2.Show()
-    rows := []gdf.Row{r, r2}
+    rows := []gdf.Row{}
 
+    for i := 1;  i < 1000000; i++ {
+        vals := make(map[string]interface{})
+        vals["A"] = i
+        vals["B"] = float64(i) * 1.2
+        vals["C"] = ":)"
+        r := gdf.NewRow(cols, vals, types)
+        rows = append(rows, r)
+    }
     df := gdf.NewDataFrame(rows, cols, types)
     df.Show()
 }
