f51b84071bf391e53689cc4a47d689e7281da47f
Owen Christie <owendot2011@gmail.com>
2020-08-21 18:36:11 -0400

    more groups

--- main.go
+++ main.go
@@ -10,18 +10,25 @@ func main() {
     rows := []gdf.Row{}
     rows2 := []gdf.Row{}
 
+    smilePad := ")"
+
     for i := 1;  i < 1000000; i++ {
+        if i % 10000 == 0 {
+            // make a bunch of groups
+            smilePad = smilePad + ")"
+        }
+
         vals := make(map[string]interface{})
         vals["A"] = i * 7
         vals["B"] = float64(i) * 1.2
-        vals["C"] = ":)"
+        vals["C"] = ":" + smilePad
         r := gdf.NewRow(cols, vals, types)
         rows = append(rows, r)
 
         vals2 := make(map[string]interface{})
         vals2["A"] = i * 13
         vals2["B"] = float64(i) * 12.33
-        vals2["C"] = ":))"
+        vals2["C"] = ":)" + smilePad
         r2 := gdf.NewRow(cols, vals2, types)
         rows2 = append(rows2, r2)
     }
